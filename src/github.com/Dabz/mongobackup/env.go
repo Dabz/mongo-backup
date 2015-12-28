ee8a276b30556bd85c25f9343bdaf1331cf45f95
Damien Gasparina <damien.gasparina@mongodb.com>
2015-12-28 22:23:24 +0100

    fixed

--- src/github.com/Dabz/mongobackup/env.go
+++ src/github.com/Dabz/mongobackup/env.go
@@ -6,122 +6,117 @@
 **
 ** Started on  Mon 28 Dec 11:31:58 2015 gaspar_d
 ** Last update Mon 28 Dec 11:40:44 2015 gaspar_d
-*/
+ */
 
 package main
 
 import (
-  "os"
-  "log"
-  "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2"
+	"log"
+	"os"
 )
 
 // global variable containing options & context informations
 type Env struct {
-  options            Options
-  homefile           *os.File
-  homeval            HomeLogFile
-  trace              *log.Logger
-  info               *log.Logger
-  warning            *log.Logger
-  error              *log.Logger
-  mongo              *mgo.Session
-  dbpath             string
-  backupdirectory    string
+	options         Options
+	homefile        *os.File
+	homeval         HomeLogFile
+	trace           *log.Logger
+	info            *log.Logger
+	warning         *log.Logger
+	error           *log.Logger
+	mongo           *mgo.Session
+	dbpath          string
+	backupdirectory string
 }
-
 
 // initialize the environment object
 func (e *Env) SetupEnvironment(o Options) {
-  traceHandle   := os.Stdout;
-  infoHandle    := os.Stdout;
-  warningHandle := os.Stdout;
-  errorHandle   := os.Stderr;
+	traceHandle := os.Stdout
+	infoHandle := os.Stdout
+	warningHandle := os.Stdout
+	errorHandle := os.Stderr
 
-  e.trace   = log.New(traceHandle, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile);
-  e.info    = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile);
-  e.warning = log.New(warningHandle, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile);
-  e.error   = log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile);
+	e.trace = log.New(traceHandle, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+	e.info = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	e.warning = log.New(warningHandle, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	e.error = log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 
-  e.options = o;
-  e.checkBackupDirectory();
-  e.checkHomeFile();
-  e.connectMongo();
-  e.setupMongo();
+	e.options = o
+	e.checkBackupDirectory()
+	e.checkHomeFile()
+	e.connectMongo()
+	e.setupMongo()
 }
-
 
 // connect to mongo
 func (e *Env) setupMongo() {
-  if (e.options.stepdown) {
-    isSec, err := e.mongoIsSecondary();
-    if (err != nil) {
-      os.Exit(1);
-    }
-    if (! isSec) {
-      e.info.Printf("Currently connected to a primary node, performing a rs.stepDown()");
-      if (e.mongoStepDown() != nil) {
-        os.Exit(1);
-      }
-    }
-  }
+	if e.options.stepdown {
+		isSec, err := e.mongoIsSecondary()
+		if err != nil {
+			os.Exit(1)
+		}
+		if !isSec {
+			e.info.Printf("Currently connected to a primary node, performing a rs.stepDown()")
+			if e.mongoStepDown() != nil {
+				os.Exit(1)
+			}
+		}
+	}
 }
-
 
 // cleanup the environment variable in case of failover
 func (e *Env) CleanupEnv() {
-  e.info.Printf("Operation failed, cleaning up the database")
-    if (e.options.fsynclock) {
-      e.info.Printf("Performing fsyncUnlock");
-    }
-  e.mongoFsyncUnLock();
-  e.homefile.Close();
+	e.info.Printf("Operation failed, cleaning up the database")
+	if e.options.fsynclock {
+		e.info.Printf("Performing fsyncUnlock")
+	}
+	e.mongoFsyncUnLock()
+	e.homefile.Close()
 }
-
 
 // find or create the backup directory
 func (e *Env) checkBackupDirectory() {
-  finfo, err := os.Stat(e.options.directory);
-  if err != nil {
-    os.Mkdir(e.options.directory, 0777);
-    finfo, err = os.Stat(e.options.directory);
-  }
+	finfo, err := os.Stat(e.options.directory)
+	if err != nil {
+		os.Mkdir(e.options.directory, 0777)
+		finfo, err = os.Stat(e.options.directory)
+	}
 
-  if err != nil {
-    e.error.Printf("can not create create %s directory (%s)", e.options.directory, err);
-    os.Exit(1);
-  } else if !finfo.IsDir() {
-    e.error.Printf("%s is not a directory", e.options.directory);
-    os.Exit(1);
-  }
+	if err != nil {
+		e.error.Printf("can not create create %s directory (%s)", e.options.directory, err)
+		os.Exit(1)
+	} else if !finfo.IsDir() {
+		e.error.Printf("%s is not a directory", e.options.directory)
+		os.Exit(1)
+	}
 }
-
 
 // find of create the home file
 func (e *Env) checkHomeFile() {
-  homefile := e.options.directory + "/backup.json";
-  _, err   := os.Stat(homefile);
+	homefile := e.options.directory + "/backup.json"
+	_, err := os.Stat(homefile)
 
-  if (err != nil) {
-    e.homefile, err = os.OpenFile(homefile, os.O_CREATE | os.O_RDWR, 0700);
-    err             = e.homeval.Create(e.homefile);
-    if err != nil {
-      e.error.Printf("can not create  %s (%s)", homefile, err);
-      os.Exit(1);
-    }
-  } else {
-    e.homefile, err = os.OpenFile(homefile, os.O_RDWR, 0700);
+	if err != nil {
+		e.homefile, err = os.OpenFile(homefile, os.O_CREATE|os.O_RDWR, 0700)
+		err = e.homeval.Create(e.homefile)
+		if err != nil {
+			e.error.Printf("can not create  %s (%s)", homefile, err)
+			os.Exit(1)
+		}
+	} else {
+		e.homefile, err = os.OpenFile(homefile, os.O_RDWR, 0700)
 
-    if err != nil {
-      e.error.Printf("can not open  %s (%s)", homefile, err);
-      os.Exit(1);
-    }
+		if err != nil {
+			e.error.Printf("can not open  %s (%s)", homefile, err)
+			os.Exit(1)
+		}
 
-    err = e.homeval.Read(e.homefile);
+		err = e.homeval.Read(e.homefile)
 
-    if (err != nil) {
-      e.error.Printf("can not parse %s (%s)", homefile, err);
-      os.Exit(1);
-    }
-  }
+		if err != nil {
+			e.error.Printf("can not parse %s (%s)", homefile, err)
+			os.Exit(1)
+		}
+	}
 }
--- src/github.com/Dabz/mongobackup/mongo.go
+++ src/github.com/Dabz/mongobackup/mongo.go
@@ -5,126 +5,123 @@
 ** Login   gaspar_d <d.gasparina@gmail.com>
 **
 ** Started on  Wed 23 Dec 23:59:53 2015 gaspar_d
-** Last update Mon 28 Dec 11:41:24 2015 gaspar_d
-*/
-
+** Last update Mon 28 Dec 19:27:02 2015 gaspar_d
+ */
 
 package main
 
 import (
-   "os"
-   "gopkg.in/mgo.v2"
-   "gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"os"
 )
 
 // create mongoclient object
 func (e *Env) connectMongo() {
-  var err error;
-  e.mongo, err = mgo.Dial(e.options.mongohost + "?connect=direct");
-  if (err != nil) {
-    e.error.Printf("Can not connect to %s (%s)", e.options.mongohost, err);
-    e.CleanupEnv();
-    os.Exit(1);
-  }
+	var err error
+	e.mongo, err = mgo.Dial(e.options.mongohost + "?connect=direct")
+	if err != nil {
+		e.error.Printf("Can not connect to %s (%s)", e.options.mongohost, err)
+		e.CleanupEnv()
+		os.Exit(1)
+	}
 
+	if e.options.mongouser != "" && e.options.mongopwd != "" {
+		err := e.mongo.DB("admin").Login(e.options.mongouser, e.options.mongopwd)
+		if err != nil {
+			e.error.Printf("Can not login with %s user (%s)", e.options.mongouser, err)
+			e.CleanupEnv()
+			os.Exit(1)
+		}
+	}
 
-  if (e.options.mongouser != "" && e.options.mongopwd != "") {
-    err := e.mongo.DB("admin").Login(e.options.mongouser, e.options.mongopwd);
-    if (err != nil) {
-      e.error.Printf("Can not login with %s user (%s)", e.options.mongouser, err);
-      e.CleanupEnv();
-      os.Exit(1);
-    }
-  }
-
-  e.mongo.SetMode(mgo.SecondaryPreferred, true);
+	e.mongo.SetMode(mgo.SecondaryPreferred, true)
 }
 
 // fetch the dbPath of the mongo instance using the
 // db.adminCommand({getCmdLineOpts: 1}) command
 func (e *Env) fetchDBPath() {
-  result := bson.M{};
-  err     := e.mongo.DB("admin").Run(bson.D{{"getCmdLineOpts", 1}}, &result);
-  if (err != nil) {
-    e.error.Printf("Can not perform command getCmdLineOpts (%s)", err);
-    e.CleanupEnv();
-    os.Exit(1);
-  }
+	result := bson.M{}
+	err := e.mongo.DB("admin").Run(bson.D{{"getCmdLineOpts", 1}}, &result)
+	if err != nil {
+		e.error.Printf("Can not perform command getCmdLineOpts (%s)", err)
+		e.CleanupEnv()
+		os.Exit(1)
+	}
 
-  e.dbpath = result["parsed"].(bson.M)["storage"].(bson.M)["dbPath"].(string);
+	e.dbpath = result["parsed"].(bson.M)["storage"].(bson.M)["dbPath"].(string)
 }
 
 // lock mongodb instance db.fsyncLock()
-func (e *Env) mongoFsyncLock() (error) {
-  result := bson.M{};
-  err    := e.mongo.DB("admin").Run(bson.D{{"fsync", 1}, {"lock", true}}, &result);
-  if (err != nil) {
-    e.error.Printf("Can not perform command fsyncLock (%s)", err);
-  }
-  return err;
+func (e *Env) mongoFsyncLock() error {
+	result := bson.M{}
+	err := e.mongo.DB("admin").Run(bson.D{{"fsync", 1}, {"lock", true}}, &result)
+	if err != nil {
+		e.error.Printf("Can not perform command fsyncLock (%s)", err)
+	}
+	return err
 }
 
 // unlock mongodb instance db.fsyncUnlock
-func (e *Env) mongoFsyncUnLock() (error) {
-  result := bson.M{};
-  err    := e.mongo.DB("admin").C("$cmd.sys.unlock").Find(bson.M{}).One(&result);
+func (e *Env) mongoFsyncUnLock() error {
+	result := bson.M{}
+	err := e.mongo.DB("admin").C("$cmd.sys.unlock").Find(bson.M{}).One(&result)
 
-  if (err != nil) {
-    e.error.Printf("Can not perform command fsyncUnlock (%s)", err);
-  }
-  return err;
+	if err != nil {
+		e.error.Printf("Can not perform command fsyncUnlock (%s)", err)
+	}
+	return err
 }
 
 // check if a mongodb instance is a secondary
 func (e *Env) mongoIsSecondary() (bool, error) {
-  result := bson.M{};
-  err    := e.mongo.DB("admin").Run(bson.D{{"isMaster", 1}}, &result);
-  if (err != nil) {
-    e.error.Printf("Can not perform command isMaster (%s)", err);
-  }
+	result := bson.M{}
+	err := e.mongo.DB("admin").Run(bson.D{{"isMaster", 1}}, &result)
+	if err != nil {
+		e.error.Printf("Can not perform command isMaster (%s)", err)
+	}
 
-  return result["secondary"].(bool), err;
+	return result["secondary"].(bool), err
 }
 
 // perform an rs.stepDown() on the connected instance
-func (e *Env) mongoStepDown() (error) {
-  result := bson.M{};
-  err    := e.mongo.DB("admin").Run(bson.D{{"replSetStepDown", 60}}, &result);
-  e.mongo.Refresh();
+func (e *Env) mongoStepDown() error {
+	result := bson.M{}
+	err := e.mongo.DB("admin").Run(bson.D{{"replSetStepDown", 60}, {"secondaryCatchUpPeriodSecs", 60}}, &result)
+	e.mongo.Refresh()
+	isSec, err := e.mongoIsSecondary()
+	if err != nil && !isSec {
+		e.error.Printf("Can not perform command replStepDown (%s)", err)
+	}
 
-  isSec, err := e.mongoIsSecondary()
-  if (err != nil && isSec) {
-    e.error.Printf("Can not perform command replSetStepDown (%s)", err);
-  }
-
-  return err;
+	return err
 }
 
 // get the last oplog entry
-func (e *Env) getOplogLastEntries() (bson.M) {
-  result := bson.M{};
-  _       = e.mongo.DB("local").C("oplog.rs").Find(bson.M{}).Sort("-$natural").One(&result);
+func (e *Env) getOplogLastEntries() bson.M {
+	result := bson.M{}
+	_ = e.mongo.DB("local").C("oplog.rs").Find(bson.M{}).Sort("-$natural").One(&result)
 
-  return result;
+	return result
 }
 
 // get the first oplog entry
-func (e *Env) getOplogFirstEntries() (bson.M) {
-  result := bson.M{};
-  _       = e.mongo.DB("local").C("oplog.rs").Find(bson.M{}).Sort("$natural").One(&result);
+func (e *Env) getOplogFirstEntries() bson.M {
+	result := bson.M{}
+	_ = e.mongo.DB("local").C("oplog.rs").Find(bson.M{}).Sort("$natural").One(&result)
 
-  return result;
+	return result
 }
 
 // get oplog entries that are greater than ts
 func (e *Env) getOplogEntries(ts bson.MongoTimestamp) (iter *mgo.Iter) {
-  query := bson.M{"ts": bson.M{"$gt": ts}};
-  iter   = e.mongo.DB("local").C("oplog.rs").Find(query).Iter()
-  return iter;
+	query := bson.M{"ts": bson.M{"$gt": ts}}
+	iter = e.mongo.DB("local").C("oplog.rs").Find(query).Iter()
+	return iter
 }
 
 // get the oplog number of entry
-func (e *Env) getOplogCount() (int) {
-  count, _ := e.mongo.DB("local").C("oplog.rs").Count();
-  return count;
+func (e *Env) getOplogCount() int {
+	count, _ := e.mongo.DB("local").C("oplog.rs").Count()
+	return count
 }
